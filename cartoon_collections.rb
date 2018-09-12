a29d7af150ac2dee2c91d6f1ba9482f1a914e3de
Allison Howard <alliemhow@gmail.com>
2018-09-12 00:37:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any?{ |word| word.length >= 4 }
+  calls.any?{ |word| word.count >= 4 }
 end
 
 def find_the_cheese# code an argument here
